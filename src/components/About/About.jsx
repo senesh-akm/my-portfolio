432a6e28c0a87953fdee993b59824aa2bfce15f4
Ranusha Senesh Akmeemana <seneshak2001@gmail.com>
2024-06-16 20:04:27 +0530

    Create a new folder and locate the related files.

---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>Write a brief introduction about yourself here.</p>
+    </section>
+  );
+}
+
+export default About;
